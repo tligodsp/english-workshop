a21c08eff0df46c29414119a0f3eedffd58f01e7
WillowWisp <37970784+WillowWisp@users.noreply.github.com>
2019-04-12 17:39:42 +0700

    Update exercise-page
    Generate ExerciseType and display Exercise onto the screen

--- src/app/exercise/exercise-page/exercise-page.component.ts
+++ src/app/exercise/exercise-page/exercise-page.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ImagePickingExerciseComponent } from '../image-picking-exercise/image-picking-exercise.component';
 import { Exercise } from '../../models/exercise';
 
+
+const TYPE_LIST = ['sentence-correcting', 'image-picking'];
+
 @Component({
   selector: 'app-exercise-page',
   templateUrl: './exercise-page.component.html',
   styleUrls: ['./exercise-page.component.css']
 })
 export class ExercisePageComponent implements OnInit {
+  currentExerciseType: string = '';
+  currentQuestionNumber: number = 1;
+  maxQuestionNumber: number = 12; // Tạm thời, sau này sẽ sửa thành @Input
+
   chosenAnswer: string;
   correctAnswers: string[] = [];
   isCorrect: boolean = null;
@@ -15,6 +22,21 @@ export class ExercisePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.generateRandomExerciseType();
+    console.log(this.getProgressWidth());
+  }
+
+  getProgressWidth() {
+    return (this.currentQuestionNumber / this.maxQuestionNumber) * 100;
+  }
+
+  generateRandomExerciseType() {
+    let randomItem;
+    do {
+      randomItem = TYPE_LIST[Math.floor(Math.random()*TYPE_LIST.length)];
+    } while (randomItem === this.currentExerciseType); // Chọn ra 1 dạng bài không trùng với dạng hiện tại
+
+    this.currentExerciseType = randomItem;
   }
 
   saveAnswer($event) {
@@ -33,7 +55,14 @@ export class ExercisePageComponent implements OnInit {
   }
 
   nextQuestion() {
-    
+    if (this.currentQuestionNumber >= this.maxQuestionNumber) {
+      console.log('Kết thúc bài học. Lưu thông tin vào localStorage và navigate tới trang chọn bài học.');
+      return;
+    }
+
+    this.currentQuestionNumber++;
+    this.isCorrect = null;
+    this.generateRandomExerciseType();
   }
 
 }
--- src/app/mock-sentences.ts
+++ src/app/mock-sentences.ts
@@ -1,11 +1,36 @@
 import { Sentence } from './models/sentence';
 
 export const SENTENCES: Sentence[] = [
-  { eng: 'A man', vie: 'một người đàn ông' },
-  { eng: 'A woman', vie: 'một người phụ nữ' },
-  { eng: 'The cat drinks milk', vie: 'con mèo uống sữa' },
-  { eng: 'A boy', vie: 'một cậu bé' },
-  { eng: 'A girl', vie: 'một cô gái' },
-  { eng: 'The boy drinks water', vie: 'cậu bé uống nước' },
-  { eng: 'I am a man', vie: 'tôi là một người đàn ông' },
+  {
+    eng: 'A man',
+    vie: ['một người đàn ông', 'một đàn ông']
+  },
+  {
+    eng: 'A woman',
+    vie: ['một người phụ nữ', 'một phụ nữ']
+  },
+  {
+    eng: 'The cat drinks milk',
+    vie: ['con mèo uống sữa', 'mèo uống sữa']
+  },
+  {
+    eng: 'A boy',
+    vie: ['một cậu bé']
+  },
+  {
+    eng: 'A girl',
+    vie: ['một cô gái', 'một cô bé', 'một bé gái']
+  },
+  {
+    eng: 'The boy drinks water',
+    vie: ['cậu bé uống nước']
+  },
+  {
+    eng: 'I am a man',
+    vie: ['tôi là một người đàn ông', 'tôi là một đàn ông', 'tôi là đàn ông']
+  },
+  {
+    eng: 'I am a woman',
+    vie: ['tôi là một người phụ nữ', 'tôi là một phụ nữ', 'tôi là phụ nữ']
+  },
 ];
