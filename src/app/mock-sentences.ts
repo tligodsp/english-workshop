745c3fd9d973316ac8ef530a52e6493417622658
lhthang1998 <lhthang.1998@gmail.com>
2019-04-14 20:16:21 +0700

    update harder exercise - 'co-ban-2'

--- src/app/mock-sentences.ts
+++ src/app/mock-sentences.ts
@@ -41,4 +41,44 @@ export const SENTENCES: Sentence[] = [
     eng: 'I am a woman',
     vie: ['tôi là một người phụ nữ', 'tôi là một phụ nữ', 'tôi là phụ nữ']
   },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'We read a newspaper',
+    vie: ['chúng tôi đọc một tờ báo', 'chúng tôi đọc tờ báo', 'chúng tôi đọc báo']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'They drink water',
+    vie: ['họ uống nước']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'I am a child',
+    vie: ['tôi là một đứa trẻ', 'tôi là một đứa bé']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'You and i drink water',
+    vie: ['bạn và tôi uống nước', 'bạn với tôi uống nước']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'We are girls',
+    vie: ['chúng tôi là những bé gái', 'chúng tôi là những cô gái']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'It is a newspaper', 
+    vie: ['nó là một tờ báo', 'nó là tờ báo']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'She has water', 
+    vie: ['cô ấy có nước', 'cô ta có nước']
+  },
+  {
+    courseKey: 'co-ban-2',
+    eng: 'We have rice', 
+    vie: ['chúng tôi có cơm', 'chúng tôi có gạo']
+  },
 ];
--- src/app/mock-vocabularies.ts
+++ src/app/mock-vocabularies.ts
@@ -10,4 +10,11 @@ export const VOCABULARIES: Vocabulary[] = [
     { courseKey: 'co-ban-1', engWord: 'milk', vieMeaning: 'sữa', illustration: 'milk.png' },
     { courseKey: 'co-ban-1', engWord: 'rice', vieMeaning: 'cơm', illustration: 'rice.png' },
     { courseKey: 'co-ban-1', engWord: 'water', vieMeaning: 'nước', illustration: 'water.png' },
+    { courseKey: 'co-ban-2', engWord: 'men', vieMeaning: 'những người đàn ông', illustration: 'men.png' },
+    { courseKey: 'co-ban-2', engWord: 'newspaper', vieMeaning: 'tờ báo', illustration: 'newspaper.png' },
+    { courseKey: 'co-ban-2', engWord: 'women', vieMeaning: 'những người phụ nữ', illustration: 'women.png' },
+    { courseKey: 'co-ban-2', engWord: 'girls', vieMeaning: 'những bé gái', illustration: 'girls.png' },
+    { courseKey: 'co-ban-2', engWord: 'sandwich', vieMeaning: 'bánh mì kẹp', illustration: 'sandwich.png' },
+    { courseKey: 'co-ban-2', engWord: 'menu', vieMeaning: 'thực đơn', illustration: 'menu.png' },
+    { courseKey: 'co-ban-2', engWord: 'book', vieMeaning: 'sách', illustration: 'book.png' },
 ];
