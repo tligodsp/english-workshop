2110833ef732ba40f9d94a2e8b8d063e03858fd2
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-06-30 17:22:57 +0700

    for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAz4aw-LsNK9gHUjKlWyYoDUrmTNuf-rtI",
+    authDomain: "english-workshop.firebaseapp.com",
+    databaseURL: "https://english-workshop.firebaseio.com",
+    projectId: "english-workshop",
+    storageBucket: "english-workshop.appspot.com",
+    messagingSenderId: "696763495852",
+    appId: "1:696763495852:web:c803e0d438221ea3"
+  }
 };
